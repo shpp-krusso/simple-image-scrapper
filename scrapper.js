6beb074fd4637728f1c11d6dbedc175dceaa7504
krusso <kstn.russo@gmail.com>
2016-12-19 23:03:45 +0200

    initial commit

---
+++ scrapper.js
@@ -0,0 +1,28 @@
+var needle = require('needle');
+var tress = require('tress');
+
+var URL = 'https://www.artlebedev.ru/kovodstvo/business-lynch/2016/';
+var results = [];
+
+// `tress` последовательно вызывает наш обработчик для каждой ссылки в очереди
+var q = tress(function(url, callback){
+
+    //тут мы обрабатываем страницу с адресом url
+    needle.get(url, function(err, res){
+        if (err) throw err;
+
+        // здесь делаем парсинг страницы из res.body
+        // делаем results.push для данных о новости
+        // делаем q.push для ссылок на обработку
+
+        callback(); //вызываем callback в конце
+    });
+});
+
+// эта функция выполнится, когда в очереди закончатся ссылки
+q.drain = function(){
+    require('fs').writeFileSync('./data.json', JSON.stringify(results, null, 4));
+};
+
+// добавляем в очередь ссылку на первую страницу списка
+q.push(URL);
